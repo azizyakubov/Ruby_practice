f2adcd29f7d2185564db52444a3d36c458f630f8
Aziz Yakubov <azizy15@gmail.com>
2018-05-07 12:08:17 -0700

    initialized linked_list project

---
+++ linked_lists/linked_lists.rb
@@ -0,0 +1,60 @@
+class Node
+  attr_accessor :value, :next_node
+
+  def initialize(value = nil, next_node = nil)
+      @value = value
+      @next_node = next_node
+  end
+end
+
+class LinkedList
+  attr_accessor :head, :tail
+
+  def append
+
+  end
+
+  def prepend
+
+  end
+
+  def size
+
+  end
+
+  def head
+
+  end
+
+  def tail
+
+  end
+
+  def at(index)
+
+  end
+
+  def pop
+
+  end
+
+  def contains?(data)
+
+  end
+
+  def find(data)
+
+  end
+
+  def to_s
+
+  end
+# EXTRA CREDIT
+  def insert_at(index)
+
+  end
+
+  def remove_at(index)
+
+  end
+end
